84e396ddb4d21e1b406fda16468541da42196493
JingpuRen <yy5396681@qq.com>
2025-03-01 03:23:15 +0800

    前后端联调功能实现

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TestComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestComponent
   }
-}
+};
 </script>
 
 <style>
